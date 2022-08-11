20eddcd12c2b7aa10ea5bafbaf12c05f940b97b6
iamvimals <vimalshetty1502@gmail.com>
2022-08-11 12:32:57 +0100

    New

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { DeviceDetectorService } from "ngx-device-detector";
-import { faInstagram, faTwitter, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { } from '@fortawesome/fontawesome-svg-core';
+import { DeviceDetectorService } from 'ngx-device-detector';
+import {
+  faInstagram,
+  faTwitter,
+  faGithub,
+  faLinkedin
+} from '@fortawesome/free-brands-svg-icons';
+import {} from '@fortawesome/fontawesome-svg-core';
 
 @Component({
   selector: 'app-home',
@@ -9,18 +14,15 @@ import { } from '@fortawesome/fontawesome-svg-core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
   isMobile: any;
   instagramIcon = faInstagram;
   twitterIcon = faTwitter;
   githubIcon = faGithub;
   linkedinIcon = faLinkedin;
-  
-  constructor(private deviceService: DeviceDetectorService) { 
+
+  constructor(private deviceService: DeviceDetectorService) {
     this.isMobile = this.deviceService.isMobile();
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
